1f8dd2bdd06b573a6400904d31065ac48501160a
DEVANSH MATHUR <186853379+Devanshmathur-git-hub@users.noreply.github.com>
2026-04-14 15:13:21 +0000

    Updated code

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,12 @@ const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 
 const server = http.createServer(app);
 
